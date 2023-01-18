5afe830a21d6afee116364f565616d1a9ac68c1e
gencery <genceryigiter@gmail.com>
2023-01-18 20:24:56 +0300

    .

--- main.js
+++ main.js
@@ -1,57 +1,53 @@
 async function getData(hat) {
-  const hsUrl = "https://openapi.izmir.bel.tr/api/eshot/hareketsaatleri/" + hat;
-  const response = await fetch(hsUrl);
-  let data = await response.json();
-  let hourList = document.getElementById("hourList");
-  data = data.HareketSaatleriHici;
+	const hsUrl = "https://openapi.izmir.bel.tr/api/eshot/hareketsaatleri/" + hat;
+	const response = await fetch(hsUrl);
+	let data = await response.json();
+	let hourList = document.getElementById("hourList");
+	data = data.HareketSaatleriHici;
 
-  let now = new Date().toLocaleTimeString("en-US", {
-    timeZone: "Europe/Istanbul",
-    hour12: false,
-  });
+	let now = new Date().toLocaleTimeString("en-US", {
+		timeZone: "Europe/Istanbul",
+		hour12: false,
+	});
+	let nowHour = Number.parseInt(now.slice(0, 2));
+	let nowMin = Number.parseInt(now.slice(3, 5));
 
-  let nowHour = Number.parseInt(now.slice(0, 2));
-  let nowMin = Number.parseInt(now.slice(3, 5));
-
-  let counter = 0;
-  createAppendElem("h3", hat);
-  for (let i = 0; i < data.length; i++) {
-    //console.log(data[i].DonusSaat);
-    let ds = data[i].DonusSaat;
-    if (
-      nowHour * 60 + nowMin <=
-      parseInt(data[i].DonusSaat.slice(0, 2)) * 60 +
-        parseInt(data[i].DonusSaat.slice(3, 5))
-    ) {
-      //document.write(data[i].DonusSaat + "<br>");
-      createAppendElem("p", data[i].DonusSaat);
-      counter++;
-      if (counter == 2) i = data.length;
-    }
-  }
+	let counter = 0;
+	createAppendElem("h3", hat);
+	for (let i = 0; i < data.length; i++) {
+		//console.log(data[i].DonusSaat);
+		let ds = data[i].DonusSaat;
+		if (
+			nowHour * 60 + nowMin <=
+			parseInt(data[i].DonusSaat.slice(0, 2)) * 60 +
+				parseInt(data[i].DonusSaat.slice(3, 5))
+		) {
+			//document.write(data[i].DonusSaat + "<br>");
+			createAppendElem("p", data[i].DonusSaat);
+			counter++;
+			if (counter == 2) i = data.length;
+		}
+	}
 }
 function createAppendElem(x, inner) {
-  let elem = document.createElement(x);
-  elem.textContent = inner;
-  document.body.appendChild(elem);
+	let elem = document.createElement(x);
+	elem.textContent = inner;
+	document.body.appendChild(elem);
 }
 
 getData("267");
 getData("505");
 getData("847");
 
-
 exchangeRateURL =
-  "https://corsanywhere.herokuapp.com/https://www.google.com/search?q=usd+try";
+	"https://corsanywhere.herokuapp.com/https://www.google.com/search?q=usd+try";
 
 let exchPara = document.createElement("p");
 
-
-
 fetch(exchangeRateURL)
-  .then((res) => res.text())
-  .then((data) => {
-    const index = data.indexOf("data-exchange-rate");
-    exchPara.innerText = "usd/try: " + data.slice(index + 20, index + 25);
-  });
+	.then((res) => res.text())
+	.then((data) => {
+		const index = data.indexOf("data-exchange-rate");
+		exchPara.innerText = "usd/try: " + data.slice(index + 20, index + 25);
+	});
 document.body.append(exchPara);
