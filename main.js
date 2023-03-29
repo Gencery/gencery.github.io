f3290de736d40aafc53782834f9dd534b5aaaf07
gencery <genceryigiter@gmail.com>
2023-03-29 03:20:24 +0300

    .

--- main.js
+++ main.js
@@ -1,53 +1,44 @@
-async function getData(hat) {
-	const hsUrl = "https://openapi.izmir.bel.tr/api/eshot/hareketsaatleri/" + hat;
-	const response = await fetch(hsUrl);
-	let data = await response.json();
-	let hourList = document.getElementById("hourList");
-	data = data.HareketSaatleriHici;
+let now = new Date();
 
-	let now = new Date().toLocaleTimeString("en-US", {
-		timeZone: "Europe/Istanbul",
-		hour12: false,
-	});
-	let nowHour = Number.parseInt(now.slice(0, 2));
-	let nowMin = Number.parseInt(now.slice(3, 5));
+function addPara(content) {
+	let para = document.createElement("p");
+	para.innerText = content;
+	document.body.append(para);
+}
 
-	let counter = 0;
-	createAppendElem("h3", hat);
-	for (let i = 0; i < data.length; i++) {
-		//console.log(data[i].DonusSaat);
-		let ds = data[i].DonusSaat;
-		if (
-			nowHour * 60 + nowMin <=
-			parseInt(data[i].DonusSaat.slice(0, 2)) * 60 +
-				parseInt(data[i].DonusSaat.slice(3, 5))
-		) {
-			//document.write(data[i].DonusSaat + "<br>");
-			createAppendElem("p", data[i].DonusSaat);
-			counter++;
-			if (counter == 2) i = data.length;
-		}
+function getDayPeriodName() {
+	let periodName = "";
+	let weekDay = now.getDay();
+
+	if (weekDay == 0) {
+		periodName = "HareketSaatleriPazar";
+	} else if (weekDay == 6) {
+		periodName = "HareketSaatleriCtesi";
+	} else {
+		periodName = "HareketSaatleriHici";
 	}
-}
-function createAppendElem(x, inner) {
-	let elem = document.createElement(x);
-	elem.textContent = inner;
-	document.body.appendChild(elem);
+
+	return periodName;
 }
 
-getData("267");
-getData("505");
-getData("847");
+function listNextHours(lineNo) {
+	fetch("https://openapi.izmir.bel.tr/api/eshot/hareketsaatleri/" + lineNo)
+		.then((res) => res.json())
+		.then((data) => {
+			let nowHoursMins = now.toTimeString().slice(0, 5);
+			let nextExpeditionHours = data[getDayPeriodName()]
+				.map((hour) => hour.DonusSaat)
+				.filter((hour) => hour >= nowHoursMins)
+				.slice(0, 2);
 
-exchangeRateURL =
-	"https://corsanywhere.herokuapp.com/https://www.google.com/search?q=usd+try";
+			addPara(lineNo);
 
-let exchPara = document.createElement("p");
+			nextExpeditionHours.forEach((hour) => {
+				addPara(hour);
+			});
+		});
+}
 
-fetch(exchangeRateURL)
-	.then((res) => res.text())
-	.then((data) => {
-		const index = data.indexOf("data-exchange-rate");
-		exchPara.innerText = "usd/try: " + data.slice(index + 20, index + 25);
-	});
-document.body.append(exchPara);
+listNextHours("505");
+listNextHours("267");
+listNextHours("847");
