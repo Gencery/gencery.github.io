21b16e7ce93d44e7b1100b4f6c84dc2a0b5a35f6
gencery <genceryigiter@gmail.com>
2023-05-20 20:45:37 +0300

    .

--- main.js
+++ main.js
@@ -43,3 +43,20 @@ listNextHours("505");
 listNextHours("267");
 listNextHours("847");
 listNextHours("152");
+
+{
+	fetch("https://www.eshot.gov.tr/tr/OtobusumNerede/290", {
+		method: "POST",
+		headers: {
+			Accept: "application/json",
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify({
+			hatId: 505,
+			durakId: 30562,
+			hatyon: 0,
+		}),
+	})
+		.then((res) => res.text())
+		.then((data) => document.write(data));
+}
