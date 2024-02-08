1c44055baead61144005eb7239c8e5169c48d4dd
Gencer <47305819+Gencery@users.noreply.github.com>
2024-02-08 14:48:50 +0300

    Add files via upload

---
+++ iklimOyun/Assets/js/scripts/main.js
@@ -0,0 +1,33 @@
+let  finishButton = document.getElementById("finishButton");
+
+//add event listener to change card content
+document.addEventListener("DOMContentLoaded", function(){
+   let cards = document.getElementsByClassName("card");
+   
+   //
+   for(let card of cards){
+      let option = card.children[0];
+      let result = card.children[1];
+
+      card.addEventListener("click", function(){
+         option.classList.add("hidden");
+         option.classList.remove("shown");
+         //
+         result.classList.add("shown");
+         result.classList.remove("hidden");
+         //
+
+      })
+   }
+   //finish game
+   finishButton.addEventListener("click", function(){
+      //
+      play.classList.remove("shown");
+      play.classList.add("hidden");
+      //
+      finish.classList.add("shown");
+      finish.classList.remove("hidden");
+      //
+   })
+})
+
