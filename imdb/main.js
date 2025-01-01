5de4b1fb4ee2f43efcf30f30ccd469bdf44132ac
gencery <genceryigiter@gmail.com>
2025-01-02 01:24:44 +0300

    .

--- imdb/main.js
+++ imdb/main.js
@@ -1,28 +1,44 @@
 function toGoogle(str) {
-  return str.split(" ").join("+") + "movie+imdb";
+  return str.split(" ").join("+") + "+movie+imdb";
 }
 
 function msg(msg) {
   document.getElementById("msg").innerText = msg;
 }
 
+function extractMovieDetails(link) {
+  const regex = /href="https:\/\/www\.imdb\.com\/title\/(tt\d+)\/".*?<h3.*?>(.*?)<\/h3>/;
+  const match = link.match(regex);
+
+  if (match) {
+    const movieId = match[1]; // The IMDb ID (e.g., tt0371746)
+    const movieName = match[2]; // The movie name (e.g., Iron Man (2008) - IMDb)
+    return { movieId, movieName };
+  }
+}
+
+function loadMovie(movieId) {
+  video.innerHTML = /*html*/`
+          <iframe src="https://vidsrc.vc/embed/movie/${movieId}"></iframe>
+        `
+}
+
 function search(query) {
+  video.innerHTML = "";
   msg("Aranıyor...");
 
   fetch(`https://api.cors.lol/?url=https://www.google.com/search?q=${encodeURIComponent(toGoogle(query))}`)
     .then(res => res.text())
     .then(data => {
-      let results = data.match(/title\/tt.*?\//ig);
-      console.log(results)
-      if (results && results.length) {
-        let id = results[0].slice(results[0].indexOf("tt") + 2,
-          - 1);
-        msg(id);
-        document.getElementById("video").setAttribute("src", `https://vidsrc.vc/embed/movie/tt${id}`)
-      }
-      else {
-        msg("Bulunamadı :(");
-      }
+      let links = data.match(/<a.*?<\/a/ig);
+      let movies = links.map(link => extractMovieDetails(link)).filter(movie => movie);
+      console.log(movies);
+
+      let resultButtons = "";
+      movies.forEach(movie =>
+        resultButtons += `<button onclick="loadMovie('${movie.movieId}')">${movie.movieName}</button>`)
+      results.innerHTML = resultButtons;
+      msg(`${movies.length} sonuç bulundu.`);
     });
 }
 
