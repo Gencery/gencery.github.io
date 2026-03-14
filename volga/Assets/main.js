7923bf40ede807c3139f31b7d10f6018c38c12d4
gencery <genceryigiter@gmail.com>
2026-03-15 00:09:31 +0300

    .

--- volga/Assets/main.js
+++ volga/Assets/main.js
@@ -18,7 +18,7 @@ async function importResources() {
 
   let responses = await Promise.all(imagesMap.map(async (image, i) => {
     try {
-      document.getElementsByTagName("main")[0].innerHTML = `<span>${i + 1}/${imgsCount}</span>`;
+      document.getElementsByTagName("main")[0].innerHTML = `<span class="loading">${i + 1}/${imgsCount}</span>`;
       return {
         res: await fetch(`./Assets/${image.src}`),
         name: image.name
