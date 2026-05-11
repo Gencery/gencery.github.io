fa3ac12d2af5390acc5613044cfe9a5e74546055
Gencer <genceryigiter@gmail.com>
2026-05-11 15:02:28 +0300

    .

--- gibi/main.js
+++ gibi/main.js
@@ -1,29 +1,331 @@
-fetch("https://www.diziizle.now/player/?v=71721", {
-  "credentials": "omit",
-  "referrer": "https://www.diziizle.now/gibi-4-sezon/bolum-4/",
-}).then(res => res.text())
-  .then(data => {
-    const regex = /videoembed\/(\d+)/;
-    const match = data.match(regex);
-
-    if (match) {
-      const videoId = match[1];
-      console.log(videoId); // Output: 9939628526291
+let episodes = {
+  "1": {
+    "1": {
+      "name": "Kokariç",
+      "length": "27:20"
+    },
+    "2": {
+      "name": "Vatka",
+      "length": "28:46"
+    },
+    "3": {
+      "name": "Nü Model",
+      "length": "30:07"
+    },
+    "4": {
+      "name": "Erasmus'la Gelen Yamyam",
+      "length": "41:07"
+    },
+    "5": {
+      "name": "Yanlış Mentor",
+      "length": "38:52"
+    },
+    "6": {
+      "name": "Karanlık Güç",
+      "length": "30:50"
+    },
+    "7": {
+      "name": "İkinci Yol",
+      "length": "42:37"
+    },
+    "8": {
+      "name": "Badana",
+      "length": "30:33"
+    },
+    "9": {
+      "name": "Ayırtma Yenilemesi",
+      "length": "38:17"
+    },
+    "10": {
+      "name": "Kan Parası",
+      "length": "36:00"
+    },
+    "11": {
+      "name": "Yılmaz Bey Banyo",
+      "length": "31:59"
+    },
+    "12": {
+      "name": "Atın Bulunuşu",
+      "length": "34:58"
     }
-  });
-
-
-//
-{
-  let seasons =
-    [...document.getElementsByTagName("tbody")]
-      .slice(1, 7)
-      .map(tbody => [...tbody.children].slice(1))
-
-  let result = {};
-  seasons.forEach((season, i) => {
-    result[i + 1] = season
-  })
-
-  console.log(result);
+  },
+  "2": {
+    "1": {
+      "name": "Vücutçu Yalvaç",
+      "length": "46:33"
+    },
+    "2": {
+      "name": "Kuki",
+      "length": "40:04"
+    },
+    "3": {
+      "name": "Bej",
+      "length": "47:28"
+    },
+    "4": {
+      "name": "Sokak Röportajı",
+      "length": "54:38"
+    },
+    "5": {
+      "name": "Çaça ve Cosplay",
+      "length": "40:45"
+    },
+    "6": {
+      "name": "İki İçi Dışı Bir Kişi",
+      "length": "42:37"
+    },
+    "7": {
+      "name": "Gelin Başı",
+      "length": "43:54"
+    },
+    "8": {
+      "name": "Resimdeki Ünlü",
+      "length": "34:55"
+    },
+    "9": {
+      "name": "Eşref Hidayet Gürdal Kültür Merkezi",
+      "length": "36:18"
+    },
+    "10": {
+      "name": "Vita Brevis",
+      "length": "48:07"
+    }
+  },
+  "3": {
+    "1": {
+      "name": "Sarışın ve Kotlu",
+      "length": "01:01:01"
+    },
+    "2": {
+      "name": "Keskin Nişancı",
+      "length": "55:00"
+    },
+    "3": {
+      "name": "Büfe",
+      "length": "43:13"
+    },
+    "4": {
+      "name": "Sınıfsal Veda",
+      "length": "51:50"
+    },
+    "5": {
+      "name": "Mazbut Bir Hayat, Sağlam Bir Ahlak, Bol Bol Sevgi",
+      "length": "45:22"
+    },
+    "6": {
+      "name": "Ghosting",
+      "length": "53:26"
+    },
+    "7": {
+      "name": "Teşekkürler",
+      "length": "41:26"
+    },
+    "8": {
+      "name": "Gülnuray",
+      "length": "44:59"
+    },
+    "9": {
+      "name": "Bilanço",
+      "length": "41:47"
+    },
+    "10": {
+      "name": "Birinci Kabak",
+      "length": "35:07"
+    }
+  },
+  "4": {
+    "1": {
+      "name": "Övgü",
+      "length": "48:24"
+    },
+    "2": {
+      "name": "Bin",
+      "length": "43:20"
+    },
+    "3": {
+      "name": "Orta Yaş Krizi",
+      "length": "53:45"
+    },
+    "4": {
+      "name": "Kafa Nakli",
+      "length": "54:24"
+    },
+    "5": {
+      "name": "Patern",
+      "length": "47:26"
+    },
+    "6": {
+      "name": "165",
+      "length": "46:29"
+    },
+    "7": {
+      "name": "Dönen Tekme",
+      "length": "41:51"
+    },
+    "8": {
+      "name": "Defter",
+      "length": "41:58"
+    },
+    "9": {
+      "name": "İç Mimari ve Dirty Talk",
+      "length": "31:14"
+    },
+    "10": {
+      "name": "MÖ 2998-2997 Eğitim Öğretim Yılı",
+      "length": "56:16"
+    }
+  },
+  "5": {
+    "1": {
+      "name": "Sinek",
+      "length": "40:08"
+    },
+    "2": {
+      "name": "Kahır",
+      "length": "46:05"
+    },
+    "3": {
+      "name": "Şako Bosphorus",
+      "length": "41:02"
+    },
+    "4": {
+      "name": "Yokluk",
+      "length": "43:35"
+    },
+    "5": {
+      "name": "Böbrek Taşı",
+      "length": "54:57"
+    },
+    "6": {
+      "name": "Aşağılık Adam Kanlar İçinde",
+      "length": "45:46"
+    },
+    "7": {
+      "name": "Leopar Ve…",
+      "length": "36:48"
+    },
+    "8": {
+      "name": "Şadan",
+      "length": "43:04"
+    },
+    "9": {
+      "name": "İrade",
+      "length": "46:15"
+    },
+    "10": {
+      "name": "Geleceğin Habercisi Genç Yetişkinler",
+      "length": "40:12"
+    },
+    "11": {
+      "name": "Kenan Ticaret",
+      "length": "36:53"
+    },
+    "12": {
+      "name": "Demir Kalenin Kuzgunları: Ölüm Büyücüsü'nün Mirası",
+      "length": "46:54"
+    },
+    "13": {
+      "name": "Demir Kalenin Kuzgunları: Ahşap Sandığın Muhafızları",
+      "length": "51:51"
+    }
+  },
+  "6": {
+    "1": {
+      "name": "Waffle",
+      "length": "52:52"
+    },
+    "2": {
+      "name": "Mikrodalga",
+      "length": "46:36"
+    },
+    "3": {
+      "name": "Girls' Night Out",
+      "length": "50:56"
+    },
+    "4": {
+      "name": "Metin",
+      "length": "46:16"
+    },
+    "5": {
+      "name": "Nomen est Omen",
+      "length": "53:01"
+    },
+    "6": {
+      "name": "Çağrı'yla Yapılan 17 Konuşma",
+      "length": "53:26"
+    },
+    "7": {
+      "name": "Gece Kaplanları",
+      "length": "50:42"
+    },
+    "8": {
+      "name": "Çöl Definesi",
+      "length": "1:00:11"
+    },
+    "9": {
+      "name": "Jumbo",
+      "length": "1:03:32"
+    },
+    "10": {
+      "name": "İyilik Sağlık",
+      "length": "54:09"
+    },
+    "11": {
+      "name": "Başkaları",
+      "length": "47:20"
+    },
+    "12": {
+      "name": "Yapılanlar: Sade Bir Tören",
+      "length": "57:30"
+    },
+    "13": {
+      "name": "Yapılanlar: Don Yılmaz'ın Seçimi",
+      "length": "1:05:29"
+    }
+  }
 }
+
+function epSelect(elem) {
+  let value = elem.value;
+  let [season, episode] = value.split("-");
+  console.log(season, episode);
+
+  fetch("https://www.diziizle.now/player/?v=71721", {
+    "credentials": "omit",
+    "referrer": `https://www.diziizle.now/gibi-${season}-sezon/bolum-${episode}/`,
+  }).then(res => res.text())
+    .then(data => {
+      const regex = /videoembed\/(\d+)/;
+      const match = data.match(regex);
+      console.log(match[1])
+    });
+}
+
+let selectMenu = /*html*/`
+  <select oninput="epSelect(this)">
+    ${Object.keys(episodes).reduce((accSeasons, curSeason) => accSeasons +/*html*/`
+      <!--SEASON-->
+      <optgroup label="${curSeason}.Sezon">
+        ${Object.keys(episodes[curSeason]).reduce((accEpisode, curEpisode) => accEpisode +/*html*/`
+        <!--EPISODE-->
+      <option value=${curSeason}-${curEpisode}>${curEpisode} - ${episodes[curSeason][curEpisode].name}</option>`, "")}
+    </optgroup>`, "")}
+  </select>
+`
+
+document.body.innerHTML = selectMenu;
+
+// fetch("https://www.diziizle.now/player/?v=71721", {
+//   "credentials": "omit",
+//   "referrer": "https://www.diziizle.now/gibi-4-sezon/bolum-4/",
+// }).then(res => res.text())
+//   .then(data => {
+//     const regex = /videoembed\/(\d+)/;
+//     const match = data.match(regex);
+
+//     if (match) {
+//       const videoId = match[1];
+//       console.log(videoId); // Output: 9939628526291
+//     }
+//   });
+
