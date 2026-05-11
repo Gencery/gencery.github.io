a99117cf10d00443df7591e1789cf32234c17ff1
gencery <genceryigiter@gmail.com>
2026-05-11 23:15:10 +0300

    .

--- gibi/main.js
+++ gibi/main.js
@@ -3,45 +3,52 @@ let episodes = {
     "1": {
       "name": "Kokariç",
       "length": "27:20",
-      "embed": "9937688398547"
+      "code": "9937688398547"
     },
     "2": {
       "name": "Vatka",
       "length": "28:46",
-      "embed": "9937714350803"
+      "code": "9937714350803"
     },
     "3": {
       "name": "Nü Model",
       "length": "30:07",
-      "embed": "9937735060179"
+      "code": "9937735060179"
     },
     "4": {
       "name": "Erasmus'la Gelen Yamyam",
-      "length": "41:07"
+      "length": "41:07",
+      "code": "9937765468883"
     },
     "5": {
       "name": "Yanlış Mentor",
-      "length": "38:52"
+      "length": "38:52",
+      "code": "9937796598483"
     },
     "6": {
       "name": "Karanlık Güç",
-      "length": "30:50"
+      "length": "30:50",
+      "code": "9937801382611"
     },
     "7": {
       "name": "İkinci Yol",
-      "length": "42:37"
+      "length": "42:37",
+      "code": "9937801841363"
     },
     "8": {
       "name": "Badana",
-      "length": "30:33"
+      "length": "30:33",
+      "code": "9937887627987"
     },
     "9": {
       "name": "Ayırtma Yenilemesi",
-      "length": "38:17"
+      "length": "38:17",
+      "code": "9937887955667"
     },
     "10": {
       "name": "Kan Parası",
-      "length": "36:00"
+      "length": "36:00",
+      "code": "9937888217811"
     },
     "11": {
       "name": "Yılmaz Bey Banyo",
@@ -55,119 +62,147 @@ let episodes = {
   "2": {
     "1": {
       "name": "Vücutçu Yalvaç",
-      "length": "46:33"
+      "length": "46:33",
+      "code": "9938014898899"
     },
     "2": {
       "name": "Kuki",
-      "length": "40:04"
+      "length": "40:04",
+      "code": "9938015161043"
     },
     "3": {
       "name": "Bej",
-      "length": "47:28"
+      "length": "47:28",
+      "code": "9938015226579"
     },
     "4": {
       "name": "Sokak Röportajı",
-      "length": "54:38"
+      "length": "54:38",
+      "code": "9938015423187"
     },
     "5": {
       "name": "Çaça ve Cosplay",
-      "length": "40:45"
+      "length": "40:45",
+      "code": "9938015554259"
     },
     "6": {
       "name": "İki İçi Dışı Bir Kişi",
-      "length": "42:37"
+      "length": "42:37",
+      "code": "9938171595475"
     },
     "7": {
       "name": "Gelin Başı",
-      "length": "43:54"
+      "length": "43:54",
+      "code": "9938171923155"
     },
     "8": {
       "name": "Resimdeki Ünlü",
-      "length": "34:55"
+      "length": "34:55",
+      "code": "9938172054227"
     },
     "9": {
       "name": "Eşref Hidayet Gürdal Kültür Merkezi",
-      "length": "36:18"
+      "length": "36:18",
+      "code": "9938172316371"
     },
     "10": {
       "name": "Vita Brevis",
-      "length": "48:07"
+      "length": "48:07",
+      "code": "9938172512979"
     }
   },
   "3": {
     "1": {
       "name": "Sarışın ve Kotlu",
-      "length": "01:01:01"
+      "length": "01:01:01",
+      "code": "9939042175699"
     },
     "2": {
       "name": "Keskin Nişancı",
-      "length": "55:00"
+      "length": "55:00",
+      "code": "9939042306771"
     },
     "3": {
       "name": "Büfe",
-      "length": "43:13"
+      "length": "43:13",
+      "code": "9939042437843"
     },
     "4": {
       "name": "Sınıfsal Veda",
-      "length": "51:50"
+      "length": "51:50",
+      "code": "9939042503379"
     },
     "5": {
       "name": "Mazbut Bir Hayat, Sağlam Bir Ahlak, Bol Bol Sevgi",
-      "length": "45:22"
+      "length": "45:22",
+      "code": "9939042699987"
     },
     "6": {
       "name": "Ghosting",
-      "length": "53:26"
+      "length": "53:26",
+      "code": "9939042962131"
     },
     "7": {
       "name": "Teşekkürler",
-      "length": "41:26"
+      "length": "41:26",
+      "code": "9939043093203"
     },
     "8": {
       "name": "Gülnuray",
-      "length": "44:59"
+      "length": "44:59",
+      "code": "9939043158739"
     },
     "9": {
       "name": "Bilanço",
-      "length": "41:47"
+      "length": "41:47",
+      "code": "9939043289811"
     },
     "10": {
       "name": "Birinci Kabak",
-      "length": "35:07"
+      "length": "35:07",
+      "code": "9939043355347"
     }
   },
   "4": {
     "1": {
       "name": "Övgü",
-      "length": "48:24"
+      "length": "48:24",
+      "code": "9939628067539"
     },
     "2": {
       "name": "Bin",
-      "length": "43:20"
+      "length": "43:20",
+      "code": "9939628198611"
     },
     "3": {
       "name": "Orta Yaş Krizi",
-      "length": "53:45"
+      "length": "53:45",
+      "code": "9939628264147"
     },
     "4": {
       "name": "Kafa Nakli",
-      "length": "54:24"
+      "length": "54:24",
+      "code": "9939628526291"
     },
     "5": {
       "name": "Patern",
-      "length": "47:26"
+      "length": "47:26",
+      "code": "9939628591827"
     },
     "6": {
       "name": "165",
-      "length": "46:29"
+      "length": "46:29",
+      "code": "9939628788435"
     },
     "7": {
       "name": "Dönen Tekme",
-      "length": "41:51"
+      "length": "41:51",
+      "code": "9939628919507"
     },
     "8": {
       "name": "Defter",
-      "length": "41:58"
+      "length": "41:58",
+      "code": "9939629050579"
     },
     "9": {
       "name": "İç Mimari ve Dirty Talk",
@@ -175,49 +210,60 @@ let episodes = {
     },
     "10": {
       "name": "MÖ 2998-2997 Eğitim Öğretim Yılı",
-      "length": "56:16"
+      "length": "56:16",
+      "code": "9939629181651"
     }
   },
   "5": {
     "1": {
       "name": "Sinek",
-      "length": "40:08"
+      "length": "40:08",
+      "code": "9941067827923"
     },
     "2": {
       "name": "Kahır",
-      "length": "46:05"
+      "length": "46:05",
+      "code": "9941067893459"
     },
     "3": {
       "name": "Şako Bosphorus",
-      "length": "41:02"
+      "length": "41:02",
+      "code": "9941068024531"
     },
     "4": {
       "name": "Yokluk",
-      "length": "43:35"
+      "length": "43:35",
+      "code": "9941066517203"
     },
     "5": {
       "name": "Böbrek Taşı",
-      "length": "54:57"
+      "length": "54:57",
+      "code": "9941066648275"
     },
     "6": {
       "name": "Aşağılık Adam Kanlar İçinde",
-      "length": "45:46"
+      "length": "45:46",
+      "code": "9941066910419"
     },
     "7": {
       "name": "Leopar Ve…",
-      "length": "36:48"
+      "length": "36:48",
+      "code": "9941067107027"
     },
     "8": {
       "name": "Şadan",
-      "length": "43:04"
+      "length": "43:04",
+      "code": "9941067238099"
     },
     "9": {
       "name": "İrade",
-      "length": "46:15"
+      "length": "46:15",
+      "code": "9941067565779"
     },
     "10": {
       "name": "Geleceğin Habercisi Genç Yetişkinler",
-      "length": "40:12"
+      "length": "40:12",
+      "code": "9941067696851"
     },
     "11": {
       "name": "Kenan Ticaret",
@@ -235,43 +281,53 @@ let episodes = {
   "6": {
     "1": {
       "name": "Waffle",
-      "length": "52:52"
+      "length": "52:52",
+      "code": "10020787915475"
     },
     "2": {
       "name": "Mikrodalga",
-      "length": "46:36"
+      "length": "46:36",
+      "code": "9941068483283"
     },
     "3": {
       "name": "Girls' Night Out",
-      "length": "50:56"
+      "length": "50:56",
+      "code": "10055641139923"
     },
     "4": {
       "name": "Metin",
-      "length": "46:16"
+      "length": "46:16",
+      "code": "9941068745427"
     },
     "5": {
       "name": "Nomen est Omen",
-      "length": "53:01"
+      "length": "53:01",
+      "code": "9941068942035"
     },
     "6": {
       "name": "Çağrı'yla Yapılan 17 Konuşma",
-      "length": "53:26"
+      "length": "53:26",
+      "code": "9941069073107"
     },
     "7": {
       "name": "Gece Kaplanları",
-      "length": "50:42"
+      "length": "50:42",
+      "code": "9941069466323"
     },
     "8": {
       "name": "Çöl Definesi",
-      "length": "1:00:11"
+      "length": "1:00:11",
+      "code": "9941069728467"
     },
     "9": {
       "name": "Jumbo",
-      "length": "1:03:32"
+      "length": "1:03:32",
+      "code": "10004801981139"
     },
     "10": {
       "name": "İyilik Sağlık",
-      "length": "54:09"
+      "length": "54:09",
+      "code": "10004802112211"
     },
     "11": {
       "name": "Başkaları",
@@ -289,19 +345,10 @@ let episodes = {
 }
 
 function epSelect(elem) {
-  let value = elem.value;
-  let [season, episode] = value.split("-");
-  console.log(season, episode);
+  let [season, episode] = elem.value.split("-");
+  let code = episodes[season][episode].code;
 
-  fetch("https://www.diziizle.now/player/?v=71721", {
-    "credentials": "omit",
-    "referrer": `https://www.diziizle.now/gibi-${season}-sezon/bolum-${episode}/`,
-  }).then(res => res.text())
-    .then(data => {
-      const regex = /videoembed\/(\d+)/;
-      const match = data.match(regex);
-      console.log(match[1])
-    });
+  document.getElementsByTagName("iframe")[0].setAttribute("src", `https://ok.ru/videoembed/${code}`);
 }
 
 let selectMenu = /*html*/`
@@ -316,20 +363,11 @@ let selectMenu = /*html*/`
   </select>
 `
 
-document.body.innerHTML = selectMenu;
+let iframe = /*html*/`
+  <iframe frameborder="0"></iframe>
+`
 
-// fetch("https://www.diziizle.now/player/?v=71721", {
-//   "credentials": "omit",
-//   "referrer": "https://www.diziizle.now/gibi-4-sezon/bolum-4/",
-// }).then(res => res.text())
-//   .then(data => {
-//     const regex = /videoembed\/(\d+)/;
-//     const match = data.match(regex);
-
-//     if (match) {
-//       const videoId = match[1];
-//       console.log(videoId); // Output: 9939628526291
-//     }
-//   });
-
-[...document.getElementsByClassName("video-card__aylki __horizontal__aylki card__xs1sd")].filter(item => item.getElementsByClassName("name__scwlw").length)
+document.body.innerHTML = /*html*/`
+  ${selectMenu}
+  ${iframe}
+`;
